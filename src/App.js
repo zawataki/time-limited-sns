f12f91b28972b948f1fb6a76e113720f6f2ecca5
Yuki Takizawa <y.takizawa64@gmail.com>
2019-12-28 10:11:44 +0900

    Add timeline

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Timeline from "./Timeline";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>時限SNS</div>
+        <div>
+          <a className="App-link" href="https://reactjs.org"
+            target="_blank" rel="noopener noreferrer">
+            Learn React
+            </a>
+        </div>
       </header>
+      <main className="App-main">
+        <Timeline />
+      </main>
     </div>
   );
 }
---
+++ src/Timeline.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import './Timeline.css';
+import moment from 'moment-timezone';
+
+function Post(props) {
+  let post = props.post;
+  let timestamp = moment().subtract(post.id, 'seconds')
+    .tz("Asia/Tokyo")
+    .format('YYYY-MM-DD HH:mm:ss');
+  return (
+    <article id={post.id} className="Post">
+      <div className="username-area">
+        <div>User ID: {post.userId}</div>
+        <div>　　</div>
+        <div>{timestamp}</div>
+      </div>
+      <div>{post.body}</div>
+    </article >
+  );
+}
+
+class Timeline extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            posts: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, posts } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+      return <div>Fetching...</div>
+    } else {
+
+      let list = [];
+      for (const post of posts) {
+        list.push(<Post key={post.id} post={post} />);
+      }
+
+      return list;
+    }
+  }
+}
+
+export default Timeline;
