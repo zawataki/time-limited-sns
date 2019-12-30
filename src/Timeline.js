9f6331f18772b4a2d0404d6172ea0252c7d02c95
Yuki Takizawa <y.takizawa64@gmail.com>
2019-12-31 08:51:08 +0900

    Fix #13
    But cannot get the author name and older data than the latest 20
    messages. So these are needed to fix.

--- src/Timeline.js
+++ src/Timeline.js
@@ -2,24 +2,26 @@ import React from 'react';
 import './Timeline.css';
 import moment from 'moment-timezone';
 import { NavLink } from "react-router-dom";
+import firebase from "./firebase";
 
 function Post(props) {
   let post = props.post;
-  let timestamp = moment().subtract(post.id, 'seconds')
-    .tz("Asia/Tokyo")
+  let timestamp = post.postedAt.tz("Asia/Tokyo")
     .format('YYYY-MM-DD HH:mm:ss');
+
   return (
     <article id={post.id} className="Post">
       <div className="Post-username-timestamp">
         <div>
-          <NavLink className="Post-username" to={'/time-limited-sns/users/' + post.userId}>
-            User: {"user" + post.userId}
+          <NavLink className="Post-username"
+            to={'/time-limited-sns/users/' + post.author.id}>
+            {post.author.name}
           </NavLink>
         </div>
         <div className="Post-timestamp">　·　</div>
         <div className="Post-timestamp">{timestamp}</div>
       </div>
-      <div>{post.body}</div>
+      <div>{post.content}</div>
     </article>
   );
 }
@@ -35,25 +37,41 @@ class Timeline extends React.Component {
   }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            posts: result
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+    let currentComponent = this;
+    const db = firebase.firestore();
+    db.collection("posted-contents")
+      .orderBy("postedAt", "desc")
+      // TODO Get old posted contents after scrolling down to the most bottom
+      .limit(20)
+      .get()
+      .then(function (querySnapshot) {
+
+        const posts = querySnapshot.docs.map(doc => {
+          // doc.data() is never undefined for query doc snapshots
+          return {
+            id: doc.id,
+            content: doc.data().body,
+            postedAt: moment.unix(doc.data().postedAt.seconds),
+            author: {
+              id: doc.data().authorID,
+              // TODO Set correct name
+              name: doc.data().authorID[0] + "さん"
+            },
+          };
+        });
+
+        currentComponent.setState({
+          isLoaded: true,
+          posts
+        });
+      })
+      .catch(function (error) {
+        console.log("Error getting documents: ", error);
+        currentComponent.setState({
+          isLoaded: true,
+          error
+        });
+      });
   }
 
   render() {
