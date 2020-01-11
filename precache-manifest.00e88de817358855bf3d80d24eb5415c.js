078cb2119b573186c9fc505b1e6bf860e49fd4e7
Yuki Takizawa <y.takizawa64@gmail.com>
2020-01-11 14:49:12 +0900

    Updates

---
+++ precache-manifest.00e88de817358855bf3d80d24eb5415c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703fba35cb820d90a219ef58eb7e305e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4945e922edb5fff3a5b4",
+    "url": "/static/css/main.ac287d42.chunk.css"
+  },
+  {
+    "revision": "25420b89b209df9524bf",
+    "url": "/static/js/2.bf563cdd.chunk.js"
+  },
+  {
+    "revision": "8980719cde979ccc1834897c78b6863a",
+    "url": "/static/js/2.bf563cdd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4945e922edb5fff3a5b4",
+    "url": "/static/js/main.82c9172f.chunk.js"
+  },
+  {
+    "revision": "b052538a5f250b307292",
+    "url": "/static/js/runtime-main.6aa06d6b.js"
+  },
+  {
+    "revision": "a4b91b6fe8f7ed13afa5b800b1c02251",
+    "url": "/static/media/delete-message.a4b91b6f.svg"
+  },
+  {
+    "revision": "f7f24d9aa36ddaf6e11078c75def2cb1",
+    "url": "/static/media/upload-image.f7f24d9a.svg"
+  }
+]);
