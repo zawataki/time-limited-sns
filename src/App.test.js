5758b0dadc993d7c74c6f4279aafd6765ac3027e
Yuki Takizawa <y.takizawa64@gmail.com>
2020-01-14 00:18:46 +0900

    Fix yarn test (Related to #17)

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,8 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { container } = render(<App />);
+  const appTextContent = container.textContent;
+  expect(appTextContent)
+    .toBe("ログイン状態を確認中です。少々お待ち下さい。（数秒かかることがあります）");
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+// Firebase Analytics requires IndexedDB to work. So set up a fake IndexedDB.
+// For more details, see https://github.com/firebase/firebase-js-sdk/issues/2336#issuecomment-550090914
+import "fake-indexeddb/auto";
